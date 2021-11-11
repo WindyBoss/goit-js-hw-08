ad5beee89fdd5e6612683574aa328959c8d403ed
Vitaliy Zhyryk <zhyryk1997@gmail.com>
2021-11-11 10:30:27 +0100

    Templates directory
    Changed the place of directory "Template"

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,7 +3,7 @@ import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-import galleryTemplate from '../template/gallery-item';
+import galleryTemplate from './template/gallery-item';
 // Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
